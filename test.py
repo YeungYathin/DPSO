37499609a8aa70406aaf32eec3fcffc4c2f533ad
杨一轩 <99703745+YeungYathin@users.noreply.github.com>
2023-04-05 00:42:05 +0800

    Final version of DPSO

---
+++ test.py
@@ -0,0 +1,22 @@
+# from input import getData
+
+
+# dataPath = '/Users/YeungYathin/Desktop/创新实践/UFLP_Datasets/ORLIB/ORLIB-uncap/70/cap71.txt'
+# m,n,f,c = getData(dataPath=dataPath)
+# print(m,n,f,c)
+
+# with open('/Users/YeungYathin/Desktop/创新实践/复现代码/PSO/DPSO_TEST/OUTPUT/test.txt', "w+") as f:
+#     f.write("aa\naa")
+# import time
+    
+# start = time.time()
+# print(0)
+# i = 0
+# while i < 10000000:
+#     i+=1
+# t = time.time()
+# print(t-start)
+
+a = open('/Users/YeungYathin/Desktop/创新实践/复现代码/PSO/M/O/MO1.opt', 'r')
+b = a.read()
+print(b)
